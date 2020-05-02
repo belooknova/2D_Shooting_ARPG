7c49c073ff1987e87801901109ec0724dcc8f900
bell <unknown.nova@outlook.jp>
2020-05-03 02:05:45 +0900

    プレイヤーをとりあえず動かせるようにした

---
+++ 2D_Shooting_ARPG/Assets/Scripts/ActionModule.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionModule : MonoBehaviour
+{
+    private Rigidbody2D rig;
+
+    [HideInInspector] //移動方向(入力)
+    public Vector2 valueMove = Vector2.zero;
+
+    [HideInInspector] //視線方向
+    public Vector2 valueDirection = Vector2.zero;
+
+    /// <summary> 接地判定に利用するBoxColliderv </summary>
+    private BoxCollider2D bc;
+
+    /// <summary> 地形のレイヤーマスク </summary>
+    [SerializeField]
+    private LayerMask groundLayerMask;
+
+    /// <summary> 接地しているかどうかを取得する </summary>
+    public bool isGround = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        if (rig == null)
+        {
+            rig = gameObject.AddComponent<Rigidbody2D>();
+            rig.gravityScale = 0.98f;
+            rig.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+
+        }
+
+        bc = GetComponent<BoxCollider2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+        Check_Ground();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rig.velocity += Vector2.up * 40;
+        }
+        
+    }
+
+    private void Move()
+    {
+        valueMove = new Vector2( Input.GetAxis("Horizontal"), 0);
+        float speed = 40;
+
+        //rig.velocity = new Vector2( 20 * valueMove.x - rig.velocity.x, rig.velocity.y);
+        rig.AddForce(60 * Vector2.right * (valueMove.x * speed - rig.velocity.x));
+
+
+    }
+
+    private void Check_Ground()
+    {
+        //Colliderと地面の距離がこの値より小さければ接地と判定する
+        float floatingDistance = 0.01f;
+
+        //  BoxCastの発生元を少し上にずらす距離
+        float epsilon = 0.005f;
+
+        Vector2 origin = new Vector2(bc.transform.position.x, bc.transform.position.y) + bc.offset + new Vector2(0f, epsilon);
+
+
+        RaycastHit2D raycastHit2 = Physics2D.BoxCast(
+            origin: origin,
+            size: bc.size,
+            angle: 0,
+            direction: Vector2.down,
+            distance: floatingDistance + epsilon,
+            layerMask: groundLayerMask
+        );
+
+        isGround = raycastHit2;
+        Debug.Log("isGround: " + isGround);
+    }
+
+}
